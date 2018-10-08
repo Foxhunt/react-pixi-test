e2a388b3ad461c706c3950a8996eb2ba01338705
Stefan Dieterle <stefand@artcom.de>
2018-10-08 14:59:41 +0200

    Change build location

--- ci/build.sh
+++ ci/build.sh
@@ -1,3 +1,3 @@
 npm install
 npm run build
-echo {\"branch\": \"$CI_BUILD_REF_NAME\", \"commit\": \"$CI_BUILD_REF\", \"pipeline\": $CI_PIPELINE_ID} > public/build.json
+echo {\"branch\": \"$CI_BUILD_REF_NAME\", \"commit\": \"$CI_BUILD_REF\", \"pipeline\": $CI_PIPELINE_ID} > dist/build.json

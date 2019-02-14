d5e9c81889f0e5a3ed110d3a2f0f0aad146ef667
Marco <marco.mojica@hotmail.de>
2019-02-14 13:41:29 +0800

    Add default eventlistener

--- src/circle.js
+++ src/circle.js
@@ -4,9 +4,17 @@ import { Graphics } from "pixi.js"
 const Circle = PixiComponent("Circle", {
   create: () => new Graphics(),
   applyProps: (g, _, props) => {
-    const { x, y, fill, line, radius, interactive, pointerdown } = props
+    const { x, y, fill, line, radius, interactive } = props
+    const {
+      pointerdown = () => {},
+      pointerover = () => {},
+      pointerout = () => {}
+    } = props
+
     g.interactive = interactive
     g.on("pointerdown", pointerdown)
+    g.on("pointerover", pointerover)
+    g.on("pointerout", pointerout)
     g.clear()
     g.beginFill(fill)
     g.lineStyle(1, line)

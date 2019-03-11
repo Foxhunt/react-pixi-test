2c188e5c0df0a179683e2107a6aa2783f99552f7
Marco <marco.mojica@hotmail.de>
2019-03-11 22:23:35 +0100

    Add recorder js

---
+++ src/Recorder.js
@@ -0,0 +1,34 @@
+import download from "downloadjs"
+
+export default class Recorder extends MediaRecorder {
+  constructor(canvas) {
+    super(canvas.captureStream(), { mimeType: "video/webm" })
+    this.chunks = []
+    this.ondataavailable = this.handleData
+  }
+
+  handleData(event) {
+    if (event.data && event.data.size > 0) {
+      this.chunks.push(event.data)
+    }
+  }
+
+  start() {
+    super.start()
+    console.log("started recording")
+  }
+
+  stop() {
+    super.stop()
+    this.exportVideo()
+    console.log("stoped recording")
+  }
+
+  exportVideo() {
+    const blob = new Blob(this.chunks, { type: "video/webm" })
+    if (blob.size > 0) {
+      download(blob, "penn.webm", "video/webm")
+      this.chunks = []
+    }
+  }
+}

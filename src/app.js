d2a76974fea16201dde5eb7c5babe2ac848ede37
Marco <marco.mojica@hotmail.de>
2019-09-21 22:12:33 +0200

    Reset

--- src/app.js
+++ src/app.js
@@ -1,89 +1,14 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import { Stage } from "@inlet/react-pixi"
-
-import Recorder from "./Recorder"
 
 import Penn from "./penn"
 
-const App = () => {
-  const [app, setApp] = useState(null)
-  const [recorder, setRecorder] = useState(null)
-  const [active, setActive] = useState(false)
-  const [conRotSpeed, setConRotSpeed] = useState(0.020)
-  const [rectPosSpeed, setRectPosSpeed] = useState(0.0005)
+const App = () =>
+  <Stage
+    width={ 800 }
+    height={ 800 } >
+    <Penn />
+  </Stage>
 
-  useEffect(() => {
-    if (app) {
-      setRecorder(new Recorder(app.view))
-    }
-  }, [app])
-
-  useEffect(() => {
-    if (active) {
-      document.title = "recording"
-      if (recorder) {
-        recorder.start()
-      }
-    } else {
-      document.title = "not recording"
-      if (recorder && recorder.state !== "inactive") {
-        recorder.stop()
-      }
-    }
-  }, [active, recorder])
-
-  useEffect(() => {
-    window.onkeydown = event => {
-      if (event.keyCode === 39) {
-        console.log("inc ConRotSpeed")
-        setConRotSpeed(val => val + 0.001)
-      }
-      if (event.keyCode === 37) {
-        console.log("dec ConRotSpeed")
-        setConRotSpeed(val => val - 0.001)
-      }
-      if (event.keyCode === 38) {
-        console.log("inc RectPosSpeed")
-        setRectPosSpeed(val => val + 0.0001)
-      }
-      if (event.keyCode === 40) {
-        console.log("dec RectPosSpeed")
-        setRectPosSpeed(val => val - 0.0001)
-      }
-      if (event.keyCode === 32) {
-        if (app) {
-          app.renderer.clear()
-        }
-      }
-    }
-    return () => {
-      window.onkeydown = null
-    }
-  })
-
-  return (
-    <>
-      <Stage
-        onClick={ () => {
-          setActive(!active)
-        } }
-        width={ 800 }
-        height={ 800 }
-        options={ {
-          antialias: true,
-          clearBeforeRender: false,
-          preserveDrawingBuffer: true
-        } }>
-        <Penn
-          active={ active }
-          conRotSpeed={ conRotSpeed }
-          rectPosSpeed={ rectPosSpeed }
-          getApp={ setApp } />
-      </Stage>
-      <div>conRotSpeed: { conRotSpeed }</div>
-      <div>rectPosSpeed: { rectPosSpeed }</div>
-    </>
-  )
-}
 
 export default App
--- src/penn.js
+++ src/penn.js
@@ -1,74 +1,10 @@
-import React, { useState, useCallback, useEffect } from "react"
-import { useApp, useTick, Graphics, Container } from "@inlet/react-pixi"
+import React from "react"
+import { Graphics, Container } from "@inlet/react-pixi"
 
-const drawRect = (g, size) => {
-  g.clear()
-  g.beginFill(0xff9900, 0.4)
-  g.lineStyle(1, 0xffffff, 0.2, 0.5)
-  g.drawCircle(0, 0, size)
-  g.endFill()
-}
+const Penn = () =>
+  <Container>
+    <Graphics />
+  </Container>
 
-const oscillate = (input, min, max) => {
-  const range = max - min
-  const out = min + Math.abs((input + range) % (range * 2) - range)
-  return out
-}
-
-const Penn = ({ active, getApp, conRotSpeed, rectPosSpeed }) => {
-  const app = useApp()
-
-  useEffect(() => {
-    getApp(app)
-  }, [])
-
-  const [contRot, setContRot] = useState(Math.PI * 1.75)
-  const [rectRot, setRectRot] = useState(0)
-  const [rectSize, setRectSize] = useState(0.3)
-  const [rectPos, setRectPos] = useState(0)
-
-  const tick = useCallback(d => {
-    if (active) {
-      setContRot(val => val + d * conRotSpeed)
-      setRectRot(val => val - d * 0.03)
-      setRectSize(val => val + d * 0.000001)
-      setRectPos(val => val + d * rectPosSpeed)
-    }
-  }, [active, conRotSpeed, rectPosSpeed])
-
-  useTick(tick)
-
-  const sizeOsc = oscillate(rectSize, 0.1, 1)
-  const posOsc = oscillate(rectPos, 0.02, 1)
-
-  const size = 30 * sizeOsc
-
-  return (
-    <Container
-      position={ [app.view.width / 2, app.view.height / 2] }
-      rotation={ contRot }>
-      <Graphics
-        interactive
-        position={ [app.view.width / 2 * posOsc, app.view.width / 2 * posOsc] }
-        rotation={ rectRot }
-        draw={ g => drawRect(g, size) } />
-      <Graphics
-        interactive
-        position={ [-app.view.width / 2 * posOsc, app.view.width / 2 * posOsc] }
-        rotation={ rectRot }
-        draw={ g => drawRect(g, size) } />
-      <Graphics
-        interactive
-        position={ [app.view.width / 2 * posOsc, -app.view.width / 2 * posOsc] }
-        rotation={ rectRot }
-        draw={ g => drawRect(g, size) } />
-      <Graphics
-        interactive
-        position={ [-app.view.width / 2 * posOsc, -app.view.width / 2 * posOsc] }
-        rotation={ rectRot }
-        draw={ g => drawRect(g, size) } />
-    </Container>
-  )
-}
 
 export default Penn

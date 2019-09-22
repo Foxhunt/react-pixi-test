2bce1f30d362d61ddee76f24b7987400b6c09b9c
Marco <marco.mojica@hotmail.de>
2019-09-22 13:17:37 +0200

    Add dynamic sizing

--- src/app.js
+++ src/app.js
@@ -1,19 +1,35 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import { Stage } from "@inlet/react-pixi"
 
 import Penn from "./penn"
 
-const App = () =>
-  <Stage
-    width={ 800 }
-    height={ 400 }
-    options={ {
-      antialias: true,
-      preserveDrawingBuffer: true,
-      clearBeforeRender: false
-    } } >
-    <Penn />
-  </Stage>
+const App = () => {
+  const [dimensions, setDimensions] = useState([window.innerWidth, window.innerHeight])
+
+  useEffect(() => {
+    function handleResize(){
+      setDimensions([window.innerWidth, window.innerHeight])
+    }
+    window.addEventListener("resize", handleResize)
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  })
+
+  return (
+    <Stage
+      width={ dimensions[0] }
+      height={ dimensions[1] }
+      options={ {
+        antialias: true,
+        preserveDrawingBuffer: true,
+        clearBeforeRender: false
+      } } >
+      <Penn />
+    </Stage>
+  )
+}
+  
 
 
 export default App

cec73b76a4fe6412ced5b780999ebb91a7289a88
Marco <marco.mojica@hotmail.de>
2019-09-22 13:18:36 +0200

    Add randomness to time

--- src/penn.js
+++ src/penn.js
@@ -22,18 +22,19 @@ const Penn = () => {
   const width = app.view.width
 
   const [time, setTime] = useState(1)
-  const [frequenz, setFrequenz] = useState(1)
+  const [frequenz, setFrequenz] = useState(-2)
   const [amplitude, setAmplitude] = useState(height / 2)
-
-  useTick(d => {
-    setTime(t => t + d * 2)
-    // setAmplitude(oscillate(time * 0.3, 0, 200))
-    // setFrequenz(oscillate(time * 0.0000001, -2, 2))
-  })
 
   const tumult = useRef(new Tumult.Simplex2())
   const rngFreq = tumult.current.gen(time * 0.0006, time * 0.0006)
   const rngAmp = tumult.current.gen(time * 0.0001, time * 0.0008)
+  const rngTime = tumult.current.gen(time * 0.00001, time * 0.0002)
+
+  useTick(d => {
+    setTime(time + d)
+    setAmplitude(oscillate(time * 0.3, 100 * rngAmp, height / 2))
+    setFrequenz(oscillate(time * 0.0000001, -2, 7))
+  })
 
   return (
     <Container
@@ -44,9 +45,9 @@ const Penn = () => {
           g => {
             g.clear()
             g.lineStyle(4, 0xff9966)
-            g.moveTo(-width / 2, amplitude * sin(-width / 2 * frequenz + time))
+            g.moveTo(-width / 2, amplitude * rngAmp * sin(-width / 2 * frequenz * rngFreq + time) * sin(-width / 2 + time * rngTime))
             for (let i = -width / 2; i <= width / 2; i++) {
-              g.lineTo(i, amplitude * rngAmp * sin(i * frequenz * rngFreq + time))
+              g.lineTo(i, amplitude * rngAmp * sin(i * frequenz * rngFreq + time) * sin(i + time * rngTime))
             }
           }
         } />
@@ -55,9 +56,9 @@ const Penn = () => {
           g => {
             g.clear()
             g.lineStyle(4, 0x6699ff)
-            g.moveTo(-width / 2, amplitude * sin(-width / 2 * frequenz + time))
+            g.moveTo(-width / 2, amplitude * sin(-width / 2 * frequenz + time * rngTime))
             for (let i = -width / 2; i <= width / 2; i++) {
-              g.lineTo(i, amplitude * sin(i * frequenz + time))
+              g.lineTo(i, amplitude * sin(i * frequenz + time * rngTime))
             }
           }
         } />

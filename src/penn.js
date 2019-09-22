f14f49634c4eff67e18b6322bc181d248d795a4b
Marco <marco.mojica@hotmail.de>
2019-09-22 02:22:05 +0200

    Add perlin noise

--- src/penn.js
+++ src/penn.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import { Graphics, Container, useTick, useApp } from "@inlet/react-pixi"
+import Tumult from "tumult"
+
 import Filter from "./Filter"
 
 function sin(x) {
@@ -20,40 +22,42 @@ const Penn = () => {
   const width = app.view.width
 
   const [time, setTime] = useState(1)
-  const [frequenz, setFrequenz] = useState(-2)
-  const [amplitude, setAmplitude] = useState(0)
+  const [frequenz, setFrequenz] = useState(5)
+  const [amplitude, setAmplitude] = useState(200)
 
   useTick(d => {
     setTime(t => t + d * 2)
-    setAmplitude(oscillate(time * 0.3, 0, 600))
-    setFrequenz(oscillate(time * 0.00001, -2, 2))
+    // setAmplitude(oscillate(time * 0.3, 0, 200))
+    // setFrequenz(oscillate(time * 0.0000001, -2, 2))
   })
+
+  const tumult = useRef(new Tumult.Simplex2())
+  const rngFreq = tumult.current.gen(time * 0.0006, time * 0.0006)
+  const rngAmp = tumult.current.gen(time * 0.0001, time * 0.0008)
 
   return (
     <Container
       position={ [width / 2, height / 2] }
       filters={ [alphaFilter] }>
       <Graphics
-        rotation={ time * 0.0001 }
         draw={
           g => {
             g.clear()
             g.lineStyle(4, 0xff9966)
             g.moveTo(-600, sin(-600 * frequenz + time) * amplitude)
             for (let i = -600; i <= 600; i++) {
-              g.lineTo(i, sin(i * frequenz + time) * amplitude)
+              g.lineTo(i, amplitude * rngAmp * sin(i * frequenz * rngFreq + time))
             }
           }
         } />
       <Graphics
-        rotation={ -time * 0.0001 }
         draw={
           g => {
             g.clear()
             g.lineStyle(4, 0x6699ff)
-            g.moveTo(-600, sin(-600 * frequenz - time) * amplitude)
+            g.moveTo(-600, sin(-600 * frequenz + time) * amplitude)
             for (let i = -600; i <= 600; i++) {
-              g.lineTo(i, sin(i * frequenz - time) * amplitude)
+              g.lineTo(i, sin(i * frequenz + time) * amplitude)
             }
           }
         } />

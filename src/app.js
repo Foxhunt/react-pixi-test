460ef25a5048b3fa5529f02af7f871e53397ccf0
Marco <marco@mojica.de>
2019-10-02 17:13:04 +0200

    Increase stage size

--- src/app.js
+++ src/app.js
@@ -37,8 +37,8 @@ const App = () => {
         {
           currentCanvas &&
           <Stage
-            width={1920}
-            height={1080}
+            width={1920 * 2}
+            height={1080 * 2}
             options={{
               antialias: true,
               preserveDrawingBuffer: true,
